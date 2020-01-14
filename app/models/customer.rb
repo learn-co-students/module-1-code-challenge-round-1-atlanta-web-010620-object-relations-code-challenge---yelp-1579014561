8cfbb4ae10df34ac48e12565193a0b415b1fcacf
Rashad Dixon <dixoncodes@gmail.com>
2020-01-14 11:07:36 -0500

    Rashad Code Challenge 1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,38 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    Review.all.select{|review| review.customer == self}
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    restaurants.count
+  end
+
+  def self.find_by_name(name)
+    all.find{|customer| customer.full_name.downcase == name.downcase}
+  end
+
+  def self.find_all_by_given_name(name)
+    all.find{|customer| customer.given_name.downcase == name.downcase}
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,29 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.collect{|review| review.customer}
+  end
+
+  def ratings
+    reviews.map{|review| review.rating}
+  end
+
+  def average_star_rating
+    ratings.inject{|a,b| a + b}.to_f/reviews.size
   end
 
 end
